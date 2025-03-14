1c545b02f4c4d164afda6184ef1702f6ff1afc2b
Bruce Li <isanoob1897@outlook.com>
2025-03-14 00:04:17 +0000

    fix variable problems

--- app/src/main/java/dev/desktop/App.java
+++ app/src/main/java/dev/desktop/App.java
@@ -5,14 +5,17 @@ import javax.script.ScriptException;
 public class App {
     public static void main(String[] args) throws ScriptException {
         try {
-            Diesel.interpret("int c = 2+4;", 1);
-            Diesel.interpret("int b = c+4;", 2);
+            // fix errors
+            Diesel.interpret("String c = \"cooler\";", 1);
+            Diesel.interpret("int b = 4+4;", 2);
             Diesel.interpret("String a = \"cool \" + \"dude\";", 3);
+            Diesel.interpret("bool k = 2!=3;", 4);
         } catch (ScriptException e) {
             throw new RuntimeException(e);
         }
         System.out.println(Diesel.intVars);
         System.out.println(Diesel.stringVars);
+        System.out.println(Diesel.boolVars);
         if (args.length != 1) {
             System.err.println("Diesel Interpreter Error!: Usage: dsl [filename]");
             System.exit(0);
--- app/src/main/java/dev/desktop/Diesel.java
+++ app/src/main/java/dev/desktop/Diesel.java
@@ -11,13 +11,14 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class Diesel {
-	// store variables
-	protected static HashMap<String, Integer> intVars = new HashMap<>();
-	protected static HashMap<String, String> stringVars = new HashMap<>();
-	protected static HashMap<String, Boolean> boolVars = new HashMap<>();
-	@SuppressWarnings("StatementWithEmptyBody")
-    public static void preprocess(String filepath) throws ScriptException{
-		// preprocess, read file, process comments, etc
+    // store variables
+    protected static HashMap<String, Integer> intVars = new HashMap<>();
+    protected static HashMap<String, String> stringVars = new HashMap<>();
+    protected static HashMap<String, Boolean> boolVars = new HashMap<>();
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public static void preprocess(String filepath) throws ScriptException {
+        // preprocess, read file, process comments, etc
         File saveFile = new File(filepath);
         List<String> content = new ArrayList<>();
         try (Scanner scanner = new Scanner(saveFile)) {
@@ -29,21 +30,23 @@ public class Diesel {
                 } else {
                     content.add(line);
                 }
-        }
+            }
         } catch (FileNotFoundException n) {
             System.err.println("Diesel Interpreter Error!: File not Found!");
             System.exit(64);
         }
         launch(content);
-	}
-	public static void launch(List<String> content) throws ScriptException{
+    }
+
+    public static void launch(List<String> content) throws ScriptException {
         int i = 0;
-        for (String line:content) {
+        for (String line : content) {
             i++;
             interpret(line, i);
         }
-	}
-        public static ArrayList<String> lex(String input) {
+    }
+
+    public static ArrayList<String> lex(String input) {
         // list of tokens in String
         ArrayList<String> result = new ArrayList<String>();
         String z = "";
@@ -52,19 +55,19 @@ public class Diesel {
         // ifs are to end strings if it hits a token
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            switch(c) {
+            switch (c) {
                 case '(':
                     if (!z.isEmpty()) {
                         result.add(z);
                         z = "";
-                    } 
+                    }
                     result.add("(");
                     break;
                 case ')':
                     if (!z.isEmpty()) {
                         result.add(z);
                         z = "";
-                    } 
+                    }
                     result.add(")");
                     break;
                 case ';':
@@ -108,11 +111,13 @@ public class Diesel {
         if (!z.isEmpty()) {
             result.add(z);
         }
-        
+
         return result;
     }
+
     public static void interpret(String line, int num) throws ScriptException {
         ArrayList<String> tokens = lex(line);
+        tokens.add(tokens.size(), "");
         try {
             String current = tokens.get(0);
             if (current.matches("int")) {
@@ -127,7 +132,7 @@ public class Diesel {
                         current = tokens.get(0);
                         if (current.matches("^[a-zA-Z0-9*/+\\-_]+$")) {
                             String value = current;
-                            for (String var:intVars.keySet()) {
+                            for (String var : intVars.keySet()) {
                                 value = value.replace(var, String.valueOf(intVars.get(var)));
                             }
                             ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
@@ -170,13 +175,45 @@ public class Diesel {
                             value = value + current;
                             tokens.remove(0);
                             current = tokens.get(0);
-                        }                        
-                        for (String var:intVars.keySet()) {
-                            value = value.replace(var, String.valueOf(intVars.get(var)));
                         }
+                        int i = 0;
+                        String result = "";
+                        boolean u = false;
+                        while (i < value.length()) {
+                            char c = value.charAt(i);
+                            if (c == '"') {
+                                u = !u;
+                            }
+                            if (u) {
+                                result += c;
+                                i++;
+                                continue;
+                            }
+                            boolean replaced = false;
+                            for (String var : intVars.keySet()) {
+                                if (value.startsWith(var, i)) {
+                                    result += intVars.get(var);
+                                    i += var.length();
+                                    replaced = true;
+                                    break;
+                                }
+                            }
+                            if (!replaced) {
+                                result += c;
+                                i++;
+                            }
+                        }
+                        value = result;
                         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
                         Object c = engine.eval(value);
                         stringVars.put(n, (String) c);
+                        tokens.remove(0);
+                        current = tokens.get(0);
+                        if (current.matches(";")) {
+                            return;
+                        } else {
+                            semicolonError(num);
+                        }
                     } else {
                         if (current.matches(";")) {
                             tokens.remove(0);
@@ -198,14 +235,24 @@ public class Diesel {
                     if (current.matches("=")) {
                         tokens.remove(0);
                         current = tokens.get(0);
-                        if (current.matches("([A-Za-z0-9\\_]+)")) {
-                            String value = current;
-                            for (String var:boolVars.keySet()) {
-                                value = value.replace(var, String.valueOf(boolVars.get(var)));
-                            }
+                        if (current.matches("true") || current.matches("false")) {
                             ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-                            Object c = engine.eval(value);
-                            boolVars.put(c, (Boolean) c);
+                            Object value = engine.eval(current);
+                            boolVars.put(n, Boolean.parseBoolean((String)value));
+                            tokens.remove(0);
+                            current = tokens.get(0);
+                            if (current.matches(";")) {
+                                return;
+                            } else {
+                                semicolonError(num);
+                            }
+                        } else if (current.matches("([A-Za-z0-9\\\\-\\\\_\\=\\!\\&\\|]+)") && !current.matches("true")
+                                && !current.matches("false")) {
+                            String value = current;
+                            for (String var : intVars.keySet()) {
+                                value = value.replace(var, String.valueOf(intVars.get(var)));
+                            }
+                            boolVars.put(n, Boolean.parseBoolean(value));    
                         } else {
                             System.err.println("Diesel Interpreter Error!: Invalid Boolean value at line " + num);
                         }
@@ -222,11 +269,12 @@ public class Diesel {
                 }
             }
         } catch (Exception e) {
-            System.err.println("Diesel Interpreter Error!: An Unknown Error Occured at line " + num);
+        System.err.println("Diesel Interpreter Error!: An Unknown Error Occured at line " + num);
         }
     }
+
     // Helper functions
-    public static String first(String str) {          
+    public static String first(String str) {
         if (str.length() < 2) {
             return str;
         } else {
